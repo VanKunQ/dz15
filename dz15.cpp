9c6b0500fc929c3e5b4387736277f89d306aa8fe
ivanan <Kudinov.vanya@bk.ru>
2021-10-11 17:32:42 +0300

    Добавьте файлы проекта.

---
+++ dz15.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+int main()
+{
+ 
+	static int N = 22;
+	for (int x = 0; x < N + 1; x++) 
+	{
+
+		if (x % 2 == 0)
+		{
+			std::cout << x << " ";
+		}
+
+		else
+		{
+			std::cout << x;
+		}
+
+	}
+
+}
+
